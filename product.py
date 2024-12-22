0a6f985a5dc0228d125b626d9a1bb13de945292c
Johnson Hsu <specialline4107@gmail.com>
2024-12-22 17:27:37 +0800

    Input the name and price of the product

---
+++ product.py
@@ -0,0 +1,12 @@
+
+product = []
+while True:
+    name = input('Please input the name of the product (type "q" to leave): ')
+    if name == 'q':
+        print('Leave the function. Thank you!')
+        break
+    price = input('Please input the price of the product: ')
+    product.append([name, price])
+
+print('You input', len(product), 'item(s)')
+print('Here is your list:', product)
