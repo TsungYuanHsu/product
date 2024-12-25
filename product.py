91da73569a6d45d83163773e591558fab9d4a027
Johnson Hsu <specialline4107@gmail.com>
2024-12-25 13:43:50 +0800

    Add function to check file is present or not

--- product.py
+++ product.py
@@ -1,17 +1,23 @@
 # This is a product recorder
 
-# Read the existing file and add more items
+import os
 
 product = []
-with open('product.csv', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if 'Name,Price' in line:
-            continue
-        product.append(line.strip().split(','))
-print(product)
+if os.path.isfile('product.csv'):
+    print('File is present')
+    
+    # Read the existing file and add more items
+    with open('product.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'Name,Price' in line:
+                continue
+            product.append(line.strip().split(','))
+    print(product)    
+
+else:
+    print('File is not present')
 
 # Let user input item
-
 while True:
     name = input('Please input the name of the product (type "q" to leave): ')
     if name == 'q':
@@ -24,12 +30,10 @@ print('You input', len(product), 'item(s)')
 print('Here is your list:', product)
 
 # Print all the items
-
 for p in product:
     print('The price of', p[0], 'is', p[1])
 
 # Write items in csv file
-
 with open('product.csv', 'w', encoding = 'utf-8') as f:
     f.write('Name' + ',' + 'Price' + '\n')
     for p in product:
