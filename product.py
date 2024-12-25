01ec3b9a8d09c7ed992234c8c221a8b471de29aa
Johnson Hsu <specialline4107@gmail.com>
2024-12-25 12:59:31 +0800

    Add read file function to continue the record

--- product.py
+++ product.py
@@ -1,6 +1,17 @@
 # This is a product recorder
 
+# Read the existing file and add more items
+
 product = []
+with open('product.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if 'Name,Price' in line:
+            continue
+        product.append(line.strip().split(','))
+print(product)
+
+# Let user input item
+
 while True:
     name = input('Please input the name of the product (type "q" to leave): ')
     if name == 'q':
@@ -12,9 +23,12 @@ while True:
 print('You input', len(product), 'item(s)')
 print('Here is your list:', product)
 
+# Print all the items
+
 for p in product:
     print('The price of', p[0], 'is', p[1])
 
+# Write items in csv file
 
 with open('product.csv', 'w', encoding = 'utf-8') as f:
     f.write('Name' + ',' + 'Price' + '\n')
